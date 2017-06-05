17cb9fc323a84c350cc594731aadfb27f7edb3d8
mvalenti <valenti.michele.w@gmail.com>
2017-06-05 16:41:14 +0200

    path_var_init folder added for quick pulling in GPU

---
+++ PATH_VAR_INIT_files/PATH_VAR_INIT_GPU_8.py
@@ -0,0 +1,111 @@
+import os
+import numpy as np
+
+PC_TRAINING_TESTING =False
+
+#FFT parameters settings
+NFFT = 2048
+hop_length_FFT = 512
+fs = 16e3
+
+label_length = int(np.floor(NFFT/2.+1))*2
+
+N_MFCC = 80
+
+context = 10
+
+#Audio truncation flag and size
+audio_reshape = 1
+audio_reshape_size = 48000
+
+# if PC_TRAINING_TESTING:
+#     current_directory = os.getcwd()
+# else:
+#     current_directory = os.getcwd()+'/Projects/MFCC_STFT_Framework'
+
+# Here we assume that the directory of 'MFCC_STFT.py' is either the same as for this file or one directory above.
+current_directory = os.path.dirname(os.path.abspath(__file__))
+if not os.path.isfile(os.path.join(current_directory, 'MFCC_STFT.py')):
+    current_directory = os.path.dirname(current_directory)
+
+NETWORK_TYPE = 'AE'
+
+dataset_name = 'Dataset'
+dataset_type = '_Clean_Speech'
+
+#Tr-Te directory. Output directory
+training_dataset_dir = current_directory+'/Audio_Test/Training_'+dataset_name+'/Training_'+dataset_name+dataset_type
+testing_dataset_dir = current_directory+'/Audio_Test/Testing_'+dataset_name+'/Testing_'+dataset_name+dataset_type
+#testing_dataset_dir=training_dataset_dir
+trained_network = current_directory+'/Networks/MFCC_STFT_Network_'+NETWORK_TYPE+'_'+dataset_name+dataset_type
+output_network_parameter_file = current_directory+'/Networks/Flue_Pipe_Network_'+NETWORK_TYPE+'_'+dataset_type+'.txt'
+output_network_parameter_file_history = current_directory+'/Networks/Flue_Pipe_Network_'+NETWORK_TYPE+'_'+dataset_type+'_HISTORY.txt'
+
+systematic_save_output_folder = current_directory+'/TRAINING_TESTING_HISTORY_SYS_SAVE'
+
+output_recap_file = 'Output_metrics_file.txt'
+history_file = 'log_training_MFCC_STFT.txt'
+
+#File format used
+audio_format = ".wav"
+# features_format = ".p"
+
+#Training FLAGS and SETTINGS
+MAKE_DATASET = 0                #If 1 dataset files are made from folders. If 0 dataset are imported from folders.
+TRAIN_TEST = 1                  #Training or testing
+VALIDATION = 1                  #If 1 we train and validate on a validation split
+
+NORMALIZATION = 1
+DENORMALIZATION = 1
+
+LOAD_MODEL = True
+
+#NETWORK AND TRAINING PARAMETERS
+optimizer = 'adamax'
+activations = 'relu'
+final_layer_activation = 'linear'
+maxEpochs = 1000
+ES_epochs = 150
+dropout = 0
+learn_rate = 1e-5
+MomentumMax = 0.9
+decay = 1e-6
+minibatch_size = 400
+batchnormalization = 1
+
+feature_range_normalization = (-1,1)
+
+CUSTOM_COST_FUNCTION = False
+
+
+if NETWORK_TYPE == 'MLP':
+
+    layer_dim = [256, 256, 256, label_length]
+    N_Layers = len(layer_dim)
+    dropout_vec = []
+
+
+if NETWORK_TYPE == 'AE':
+
+    frame_length = 2048
+
+    audio_reshape_size = frame_length*(audio_reshape_size/frame_length)
+
+    layer_dim = [128, 128]
+    N_layers_MLP = len(layer_dim)
+    N_filters = [32, 64, 128]
+    N_Layers_Conv = len(N_filters)
+    CNN_kernel_sizes = [5, 5, 5]
+    maxPooling = [2, 2, 2]
+
+    N_Layers = len(layer_dim)
+
+if NETWORK_TYPE == 'CNN':
+
+    layerSizes = [4, 4]
+    poolsize = [[-1,-1], [-1,-1], [-1,-1], [-1,-1], [-1,-1], [-1,-1], [-1,-1]]
+    last_max_pooling_over_time = -1
+    receptiveField = [[2,2], [2,2], [2,2], [2,2], [2,2], [2,2], [2,2], [2,2]]
+
+    CNN_layerSize = [4, 4, 8, 8, -1, -1, -1]
+    dense_final_layer_size = [64, -1, -1, label_length]
